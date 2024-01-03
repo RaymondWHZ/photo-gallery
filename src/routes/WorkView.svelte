774950a8a63da11d7b1531191d50af20b72e6321
RaymondWHZ <wuhaozhen1106@gmail.com>
2024-01-02 22:41:37 -0600

    feat: add right image layout

--- src/routes/WorkView.svelte
+++ src/routes/WorkView.svelte
@@ -47,4 +47,48 @@
 			</div>
 		</div>
 	{/if}
+	{#if work.display === 'right'}
+		<div class="flex flex-row items-end w-full gap-16">
+			<div class="flex flex-col items-start gap-3">
+				<p class="text-xs font-light text-gray-500">Randomly chosen to be displayed</p>
+				<div class="flex flex-row items-baseline gap-6">
+					<h2 class="text-3xl">{work.title}</h2>
+					<h3>{work.shutter} f{work.aperture} {work.iso}</h3>
+				</div>
+				<p class="text-xs font-light text-gray-500 w-[350px]">{work.description}</p>
+				<p class="text-xs font-light italic text-gray-500">
+					Shot on {work.date} at {work.locationName} <br/>
+					with {work.deviceName} {#if work.lensName}+ {work.lensName}{/if}
+				</p>
+				<span class="text-3xl font-reenie-beanie">
+					Raymond Wu
+				</span>
+			</div>
+			<div class="shrink">
+				<img src={work.imageUrl} alt={work.title} />
+			</div>
+		</div>
+	{/if}
+	{#if work.display === 'middle'}
+		<div class="flex flex-row items-start w-full gap-16">
+			<div class="flex flex-col items-start gap-3">
+				<p class="text-xs font-light text-gray-500">Randomly chosen to be displayed</p>
+				<h2 class="text-3xl">{work.title}</h2>
+				<h3>{work.shutter} f{work.aperture} {work.iso}</h3>
+				<p class="text-xs font-light text-gray-500 w-[280px]">{work.description}</p>
+			</div>
+			<div class="shrink">
+				<img src={work.imageUrl} alt={work.title} />
+			</div>
+			<div class="flex flex-col items-start justify-end gap-3 h-full">
+				<p class="text-xs font-light italic text-gray-500 w-[280px]">
+					Shot on {work.date} at {work.locationName} <br/>
+					with {work.deviceName} {#if work.lensName}+ {work.lensName}{/if}
+				</p>
+				<span class="text-3xl font-reenie-beanie">
+					Raymond Wu
+				</span>
+			</div>
+		</div>
+	{/if}
 </div>

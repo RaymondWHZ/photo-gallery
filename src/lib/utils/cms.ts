d7282a1684bbe200d3753c5de590f8440c4823b2
RaymondWHZ <wuhaozhen1106@gmail.com>
2024-01-03 10:01:23 -0600

    feat: filter published documents

--- src/lib/utils/cms.ts
+++ src/lib/utils/cms.ts
@@ -4,7 +4,8 @@ const client = createClient({
 	projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
 	dataset: import.meta.env.VITE_SANITY_DATASET,
 	useCdn: true,
-	apiVersion: '2023-05-03'
+	apiVersion: '2023-05-03',
+	perspective: 'published'
 });
 
 export interface WorkOverview {

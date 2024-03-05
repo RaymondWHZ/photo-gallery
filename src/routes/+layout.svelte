89dde4e52e08519d4cd88ba105b5cb5eea53351e
RaymondWHZ <wuhaozhen1106@gmail.com>
2024-03-05 01:17:31 -0600

    fix: main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,19 +5,23 @@
 	import { PUBLIC_GITHUB_REPO_URL } from '$env/static/public';
 </script>
 
-<div class="px-4 sm:px-10 lg:px-40 h-[100vh] flex flex-col justify-stretch items-center">
-	<div class="w-full max-w-[1200px]">
-		<Header />
-	</div>
+<div class="h-[100vh] flex flex-col justify-stretch items-stretch">
+	<nav class="px-4 sm:px-10 lg:px-40 flex flex-row justify-center">
+		<div class="w-full max-w-[1200px]">
+			<Header />
+		</div>
+	</nav>
 
-	<main class="-px-4 sm:-px-10 lg:-mx-40 grow w-screen">
+	<main class="grow">
 		<slot />
 	</main>
 
-	<footer class="w-full max-w-[1200px]">
-		<p class="text-gray-400 text-xs py-10">
-			© 2023 Raymond Wu. All rights reserved. Applies to all contents.
-			<a class="underline" href={PUBLIC_GITHUB_REPO_URL}>Github</a>.
-		</p>
+	<footer class="px-4 sm:px-10 lg:px-40 flex flex-row justify-center">
+		<div class="w-full max-w-[1200px]">
+			<p class="text-gray-400 text-xs py-10">
+				© 2023 Raymond Wu. All rights reserved. Applies to all contents.
+				<a class="underline" href={PUBLIC_GITHUB_REPO_URL}>Github</a>.
+			</p>
+		</div>
 	</footer>
 </div>
